8029e5a68ec40e0b4faff274365bac97eb6d0678
joaco844 <joacojoafra@gmail.com>
2026-08-28 16:58:13 -0300

    clase tarea

---
+++ src/Tarea.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package src;
+/**
+ *
+ * @author joacodiaz
+ */
+public class Tarea {
+    private String nombre;
+    private boolean completada;
+
+    public Tarea(String nombre) {
+        this.nombre = nombre;
+        this.completada = false;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public boolean estaCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
+}
+
